4241f01ae3481b388d7e2aac2fb3636a3f55243f
mak <mkubatkin@yandex.ru>
2025-10-19 23:17:19 +0300

    bez agregatorov

--- include/utils.hpp
+++ include/utils.hpp
@@ -7,6 +7,9 @@
 
 inline int ToInt(std::string_view value) {
     int result{};
+    if ((value.size()>1) && value[0] == ' ') {
+        value = value.substr(1);
+    }
     auto [parse_end_ptr, error_code] = std::from_chars(value.begin(), value.end(), result);
     if (error_code != std::errc{} || parse_end_ptr != value.data() + value.size()) {
         throw std::invalid_argument("Cannot convert '" + std::string(value) + "' to integral");
--- src/metric.cpp
+++ src/metric.cpp
@@ -23,12 +23,17 @@
 namespace analyser::metric {
 
 void MetricExtractor::RegisterMetric(std::unique_ptr<IMetric> metric) {
-    // здесь ваш код
+    metrics.push_back(std::move(metric));
 }
 
 MetricResults MetricExtractor::Get(const function::Function &func) const {
-    // здесь ваш код
-    return {};
+    MetricResults results;
+    
+    for (const auto& metric : metrics) {
+        results.push_back(metric->Calculate(func));
+    }
+    
+    return results;
 }
 
 }  // namespace analyser::metric
